a69e1567103142b64d62e9e9653ad3ffefa40b0b
muniahmed <47899291+muniahmed@users.noreply.github.com>
2024-08-07 14:31:07 -0400

    Completed CodeEditor

---
+++ .eslintrc.cjs
@@ -0,0 +1,28 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+    "plugin:react-hooks/recommended",
+  ],
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  parserOptions: { ecmaVersion: "latest", sourceType: "module" },
+  settings: { react: { version: "18.2" } },
+  plugins: ["react-refresh"],
+  rules: {
+    "react/jsx-no-target-blank": "off",
+    "react-refresh/only-export-components": [
+      "warn",
+      { allowConstantExport: true },
+    ],
+    "no-unused-vars": [
+      "warn", // Show warnings instead of errors for unused variables
+      { vars: "all", args: "after-used", ignoreRestSiblings: true },
+    ],
+    "react/prop-types": "off", // Turn off prop-types rule if not using PropTypes
+    "no-console": "off", // Allow console statements for debugging
+    "react/react-in-jsx-scope": "off", // Disable for React 17+ or Next.js with JSX automatic runtime
+  },
+};
